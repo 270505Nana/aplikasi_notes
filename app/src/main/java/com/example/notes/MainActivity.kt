db85b535aa6abb4a887586396c01d74f2fb9bf0d
nana <prajna270505@gmail.com>
2021-11-19 19:24:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.notes
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide() //ini kaya efek gtu
+
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity, Login::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+
+    }
+}
